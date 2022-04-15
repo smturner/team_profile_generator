52eead8f34a4e997ba2d621a591be1c44f615ce6
Sarah Turner <turnersarah228@gmail.com>
2022-04-15 17:43:34 -0500

    added engineer and intern functions to generateHTML file

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # team_profile_generator
 
+in index.js create an array to store all of your employees
+
+    5. create function to initialize the app
+        --divide it up based on employee
+            --inquire prompts split up by role
+            --some that are universal
+
+                 --all teams need at least one manager, create a nested function to generate a manager with inquire prompts
+                      --create a new Manager object using the manager class and the data from the inquier prompts
+                      --push that object to the array of employees
+            
+            --do the same thing for Intern, and engineer 
+                --set them up as separate functions within the bigger function
+            
+            --use a prompt to ask if you'd like to create another employee or finish generating your team--use conditionals for logic
+                --switch statement or if else statements
+
+            --create a function that stops the inquirer prompts and generates the html page
+                -use fs to write this file to the dist folder
+                
+
+6.  create a helper function in your src folder to generate HTML file (smilar to generateMarkdown in your README generator)
+    -generate employee ccards for each type of employee, pass in the data from inquier prompts
+    -create an array to store the html, and then push html for each employee into the array and return as a string at the end
+    -make sure you're exporting this function and importing it to your index file
+
+7.
+
+
+
 
 1. create an index.js that prompts a user to ask questions
 2. use inquierer to ask questions and store the information
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,83 +1,131 @@
-
-
-<body>
-    <div class= "container">
-        <div class = "row">
+const generateManager = () => {
+    return `
     <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
-                <h3>Name</h3>
-                <h4>Job Title</h4>
-            </div>
-            <div class="card-body">
-                <p class="id">An item</p>
-                <p class="email">A second item</p>
-                <p class="officeNumber">A third item</p>
-            </div>
-
+    <div class="card h-100">
+        <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
+            <h3>${manager.name}</h3>
+            <h4>Manager</h4>
+        </div>
+        <div class="card-body">
+            <p class="id">ID: ${manager.id}</p>
+            <p class="email">Email: ${manager.email}</p>
+            <p class="officeNumber">Office Number: ${manager.officeNumber}</p>
         </div>
     </div>
-    <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
-                <h3>Name</h3>
-                <h4>Job Title</h4>
-            </div>
-            <div class="card-body">
-                <p class="id">An item</p>
-                <p class="email">A second item</p>
-                <p class="officeNumber">A third item</p>
-            </div>
-
-        </div>
-    </div>
-    <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
-                <h3>Name</h3>
-                <h4>Job Title</h4>
-            </div>
-            <div class="card-body">
-                <p class="id">An item</p>
-                <p class="email">A second item</p>
-                <p class="officeNumber">A third item</p>
-            </div>
-
-        </div>
-    </div>
-    <div class="col-2"></div>
-    <div class="col-4 mt-4">
-        <div class="card h-100">
-            <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
-                <h3>Name</h3>
-                <h4>Job Title</h4>
-            </div>
-            <div class="card-body">
-                <p class="id">An item</p>
-                <p class="email">A second item</p>
-                <p class="officeNumber">A third item</p>
-            </div>
-
-        </div>
-    </div>
+</div>
+    `
     
+}
+
+const generateEngineer = () => {
+    return`
+    <div class="col-4 mt-4">
+    <div class="card h-100">
+        <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
+            <h3>${engineer.name}</h3>
+            <h4>Engineer</h4>
+        </div>
+        <div class="card-body">
+            <p class="id">${engineer.id}</p>
+            <p class="email">${engineer.email}</p>
+            <p class="gitHub">${engineer.gitHub}</p>
+        </div>
+    </div>
+</div>
+    `
+}
+
+const generateIntern = () => {
+    return `
     <div class="col-4 mt-4">
         <div class="card h-100">
             <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
-                <h3>Name</h3>
-                <h4>Job Title</h4>
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4>
             </div>
             <div class="card-body">
-                <p class="id">An item</p>
-                <p class="email">A second item</p>
-                <p class="officeNumber">A third item</p>
+                <p class="id">${intern.id}</p>
+                <p class="email">${intern.email}</p>
+                <p class="school">${intern.school}</p>
             </div>
-
         </div>
     </div>
-    <div class="col-2"></div>
-</div>
-</div>
-</body>
+    `
+}
+//     <div class= "container">
+//         <div class = "row">
+//     <div class="col-4 mt-4">
+//         <div class="card h-100">
+//             <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
+//                 <h3>Name</h3>
+//                 <h4>Job Title</h4>
+//             </div>
+//             <div class="card-body">
+//                 <p class="id">An item</p>
+//                 <p class="email">A second item</p>
+//                 <p class="officeNumber">A third item</p>
+//             </div>
 
-</html>
+//         </div>
+//     </div>
+//     <div class="col-4 mt-4">
+//         <div class="card h-100">
+//             <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
+//                 <h3>Name</h3>
+//                 <h4>Job Title</h4>
+//             </div>
+//             <div class="card-body">
+//                 <p class="id">An item</p>
+//                 <p class="email">A second item</p>
+//                 <p class="officeNumber">A third item</p>
+//             </div>
+
+//         </div>
+//     </div>
+//     <div class="col-4 mt-4">
+//         <div class="card h-100">
+//             <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
+//                 <h3>Name</h3>
+//                 <h4>Job Title</h4>
+//             </div>
+//             <div class="card-body">
+//                 <p class="id">An item</p>
+//                 <p class="email">A second item</p>
+//                 <p class="officeNumber">A third item</p>
+//             </div>
+
+//         </div>
+//     </div>
+//     <div class="col-2"></div>
+//     <div class="col-4 mt-4">
+//         <div class="card h-100">
+//             <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
+//                 <h3>Name</h3>
+//                 <h4>Job Title</h4>
+//             </div>
+//             <div class="card-body">
+//                 <p class="id">An item</p>
+//                 <p class="email">A second item</p>
+//                 <p class="officeNumber">A third item</p>
+//             </div>
+
+//         </div>
+//     </div>
+    
+//     <div class="col-4 mt-4">
+//         <div class="card h-100">
+//             <div class="card-header" style="background-color:rgb(231, 170, 129); color:white;">
+//                 <h3>Name</h3>
+//                 <h4>Job Title</h4>
+//             </div>
+//             <div class="card-body">
+//                 <p class="id">An item</p>
+//                 <p class="email">A second item</p>
+//                 <p class="officeNumber">A third item</p>
+//             </div>
+
+//         </div>
+//     </div>
+//     <div class="col-2"></div>
+// </div>
+// </div>
